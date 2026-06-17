e6ef51aa9d81d2386b0fcd8607512f8596dc5dac
无常 <zhangtao@papegames.net>
2026-06-17 09:38:53 +0800

    Initial Commit

---
+++ nas-music/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  nas-music
+//
+//  Created by wuchang on 2026/6/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ nas-music/nas_musicApp.swift
@@ -0,0 +1,17 @@
+//
+//  nas_musicApp.swift
+//  nas-music
+//
+//  Created by wuchang on 2026/6/17.
+//
+
+import SwiftUI
+
+@main
+struct nas_musicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
